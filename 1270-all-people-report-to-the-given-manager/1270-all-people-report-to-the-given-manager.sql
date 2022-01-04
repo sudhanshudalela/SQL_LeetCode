788dbdd5c26be035eee30ccdd26898884ed3c2ee
Sudhanshu Dalela <47723728+sudhanshudalela@users.noreply.github.com>
2022-01-04 12:58:33 -0800

    Time: 241 ms (48.50%), Space: 0B (100.00%) - LeetHub

---
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+    
+SELECT employee_id
+FROM Employees
+WHERE (manager_id = 1 AND employee_id != 1) 
+    OR (manager_id IN (SELECT employee_id FROM Employees WHERE manager_id = 1 AND employee_id != 1))
+    OR manager_id IN (SELECT employee_id FROM Employees WHERE manager_id IN (SELECT employee_id FROM Employees WHERE manager_id = 1 AND employee_id != 1))
